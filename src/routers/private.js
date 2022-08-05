35ec3465da1b1e7def2cbc08c0ecea05bc42c6ac
nassimna <najjarnassim1995@gmail.com>
2022-08-05 14:24:03 +0100

    modified:   .firebase/hosting.YnVpbGQ.cache 	modified:   build/asset-manifest.json 	modified:   build/index.html 	renamed:    build/static/js/main.e5d729e0.js -> build/static/js/main.151a9ddd.js 	renamed:    build/static/js/main.e5d729e0.js.LICENSE.txt -> build/static/js/main.151a9ddd.js.LICENSE.txt 	new file:   build/static/js/main.151a9ddd.js.map 	deleted:    build/static/js/main.e5d729e0.js.map 	modified:   src/routers/private.js 	modified:   src/routers/public.js

--- src/routers/private.js
+++ src/routers/private.js
@@ -3,19 +3,21 @@ import { Route, Redirect } from "react-router-dom";
 export const PrivateRoute = ({ component: Component, roles, ...rest }) => (
   <Route
     {...rest}
-    render={(props) => {
-      const user = JSON.parse(localStorage.getItem("profile"));
-      const token = user?.access;
-
-      if (!token) {
-        return <Redirect to="/login" />;
+    render={
+      (props) => {
+        const user = JSON.parse(localStorage.getItem("profile"));
+        const token = user?.access;
+        if (!token) {
+          return <Redirect to="/login" />;
+        }
+        if (token) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to="/login" />;
+        }
       }
 
-      if (token) {
-        return <Component {...props} />;
-      } else {
-        return <Redirect to="/login" />;
-      }
-    }}
+      // authorised so return component
+    }
   />
 );
--- src/routers/public.js
+++ src/routers/public.js
@@ -3,16 +3,16 @@ import { Route, Redirect } from "react-router-dom";
 export const PublicRoute = ({ component: Component, restricted, ...rest }) => {
   const user = JSON.parse(localStorage.getItem("profile"));
   const token = user?.access;
-
   return (
+    // restricted = false meaning public route
+    // restricted = true meaning restricted route
     <Route
       {...rest}
       render={(props) => {
-        if (!token) {
-          return <Component {...props} />;
-        }
-        if (token && restricted) {
-          return <Redirect to="/" />;
+        if (token) {
+          if (token && restricted) {
+            return <Redirect to="/" />;
+          }
         } else {
           return <Component {...props} />;
         }
